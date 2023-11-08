c7e0f92ad6acec44667233549214cd712f505401
Baxshullayev12 <baxshullayevd12@gmail.com>
2023-11-08 02:30:08 -0800

    12

--- logic12.py
+++ logic12.py
@@ -1,9 +1,17 @@
-def main(a):
-    """
+#def main(a):
+"""
     Given a two-digit integer a,  check the following statement "All digits of the number are the same".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
+#    x = str(a)
+#    return x[0]==x[1]
+#print(main(22))
+
+def main(a):
+    if a%10==a//10:
+        return True
+    return False
+print(main(55))
